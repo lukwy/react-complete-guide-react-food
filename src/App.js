c497fe63d19418570d0cbc9baca920f7145e21c5
lukwyp <wypchlo.lukasz@gmail.com>
2023-03-10 14:15:08 +0100

    Added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Header from "./componenets/Layout/Header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
