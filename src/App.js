00d37b322d708e850f98130b3f8f88b5b30c2f5c
lukwyp <wypchlo.lukasz@gmail.com>
2023-03-10 19:44:43 +0100

    Added cart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { Fragment } from "react";
+import { useState } from "react";
 import Header from "./componenets/Layout/Header";
 import Meals from "./componenets/Meals/Meals";
+import Cart from "./componenets/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <Fragment>
-      <Header />
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/componenets/Layout/HeaderCardButton.js
+++ src/componenets/Layout/HeaderCardButton.js
@@ -1,16 +1,25 @@
-import classes from './HeaderCardButton.module.css';
-import CartIcon from '../Cart/CartIcon';
+import { useContext } from "react";
 
-const HeaderCardButton = props => {
-    return <button className={classes.button}>
-        <span className={classes.icon}>
-            <CartIcon />
-        </span>
-        <span>Your Cart</span>
-        <span className={classes.badge}>
-            3
-        </span>
+import classes from "./HeaderCardButton.module.css";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+
+const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
+  );
 };
 
 export default HeaderCardButton;
