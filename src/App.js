07b09727efd736ef67cb6c6e3ccbc909deca9385
lukwyp <wypchlo.lukasz@gmail.com>
2023-03-10 14:30:25 +0100

    Added Meals list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./componenets/Layout/Header";
+import Meals from "./componenets/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
