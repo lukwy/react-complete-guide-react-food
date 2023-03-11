56cbf0a59093b788640458f878955ad4c0fa6b6b
lukwyp <wypchlo.lukasz@gmail.com>
2023-03-11 16:55:19 +0100

    Added cart items

--- src/componenets/Layout/HeaderCardButton.js
+++ src/componenets/Layout/HeaderCardButton.js
@@ -1,18 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import classes from "./HeaderCardButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCardButton = (props) => {
+  const [btnIsHighlited, setBtnIsHighlited] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsHighlited ? classes.bump : ""} `;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlited(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlited(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
